1f3e900f6a04fbd860d3ff4946b92b46f96b5b3a
Justas Stankevicius <juuuuuustas@gmail.com>
2025-05-11 11:21:56 -0400

    Basic mutation

--- seed/evolution/ast_generator.py
+++ seed/evolution/ast_generator.py
@@ -34,6 +34,7 @@ import random
 from copy import deepcopy
 from dataclasses import dataclass
 from typing import Type
+from pprint import pprint
 
 
 class Bar:
@@ -43,6 +44,7 @@ class Bar:
     def bar_method(self) -> bool:
         return True
 
+
 class Foo:
 
     def __init__(self):
@@ -51,8 +53,14 @@ class Foo:
     def returns_int(self) -> int:
         return 1
 
-    def returns_bool(self) -> bool:
+    def returns_another_int(self) -> int:
+        return 2
+
+    def returns_true(self) -> bool:
         return True
+
+    def returns_false(self) -> bool:
+        return False
 
     def returns_list_of_ints(self) -> list[int]:
         return [1, 2]
@@ -113,8 +121,8 @@ def __make_dummy_func(name, params, return_annotation):
 
 __DUNDER_METHOD_SIGNATURE_OVERRIDES = {
     int: [
-        __make_dummy_func("__add__", [("a", int)], int),
-        __make_dummy_func("__add__", [("a", float)], float),
+        __make_dummy_func("__add__", [("self", inspect._empty), ("a", int)], int),
+        __make_dummy_func("__add__", [("self", inspect._empty), ("a", float)], float),
     ],
     float: [],
 }
@@ -154,13 +162,16 @@ class Scope:
 
         self._vars[var_name] = var_type
 
+    def get_var_type(self, var_name: str) -> type:
+        return self._vars[var_name]
+
     def get_calls_with_predicate(self, predicate):
         res = []
         predicate = predicate or (lambda x: True)
 
         for var_name, var_type in self._vars.items():
             methods = [
-                (var_name, method_name, method)
+                (var_name, method)
                 for method_name, method in _get_methods(var_type)
                 if is_allowed_method(var_type, method_name) and predicate(method)
             ]
@@ -183,6 +194,18 @@ class Scope:
         else:
             return [(var, ty) for var, ty in self._vars.items() if ty == term_type]
 
+    def get_all_types(self) -> list[type]:
+        calls = self.get_call_expressions()
+        terminals = self.get_terminals()
+
+        call_types = [
+            inspect.signature(method).return_annotation for _, method in calls
+        ]
+
+        terminal_types = [ty for _, ty in terminals]
+
+        return call_types + terminal_types
+
 
 def get_container_inner_type(container_type):
     # Check if `list_type` is actually parametrized
@@ -199,7 +222,12 @@ def get_container_inner_type(container_type):
 
 
 MAX_SCOPES = 4
-MAX_EXPR_DEPTH = 3
+
+BASE_TERMINAL_PROBABILITY = 0.5
+MAX_EXPR_DEPTH = 4
+
+# Don't allow for anything worse than Ω(n^2)
+MAX_LOOP_DEPTH = 2
 
 PROB_IF_STMT_CONTAINS_ELSE = 0.5
 PROB_BOOL_EXPR_GETS_BOOL_OP = 0.2
@@ -212,20 +240,52 @@ class NodeInfo:
     # node was created, because the Scope object might contain references to variables
     # that were defined after this node.
     scope: Scope
-    expr_type: type
+    expr_type: type | None
 
 
-class ASTGenerator:
+def _method_has_no_parameters(method) -> bool:
+    params = inspect.signature(method).parameters
+    if len(params) < 0:
+        raise AssertionError(
+            f"Method {method.__name__} has 0 parameters when it"
+            "should have at least 'self'!"
+        )
+
+    if "self" not in params:
+        raise AssertionError(
+            f"Method {method.__name__} has multiple parameters, but"
+            "none of them are 'self'!"
+        )
+
+    if "self" != list(params.keys())[0]:
+        raise AssertionError(
+            f"'self' is a parameter in {method.__name__}, but it is"
+            "not the first parameter!"
+        )
+
+    return len(params) == 1
+
+
+class ASTGenerator(ast.NodeTransformer):
 
     def __init__(self):
+        self.root = None
+
         self.node_metadata = {}
         self.scopes = []
+
+        # Stats
         self.expr_depth = 0
+        self.loop_depth = 0
         self.var_idx = 0
+
+        # Constant pool (we might want to mutate them later or something)
+        self.literals_by_type = {int: [1, 2], float: [1.0]}
 
         # Set up global scope
         self.enter_scope()
-        self.add_var_to_current_scope("foo", Foo)
+        self.add_var("foo", Foo)
+
 
     def enter_scope(self) -> None:
         if not self.scopes:
@@ -244,21 +304,26 @@ class ASTGenerator:
         self.var_idx += 1
         return name
 
-    def add_var_to_current_scope(self, var_name: str, var_type: Type) -> None:
+    def add_var(self, var_name: str, var_type: Type) -> None:
         # TODO: Maybe we should allow for actual values? Type is implicit.
         self.cur_scope().add_var(var_name, var_type)
 
-    def gen_method_call(self, var_name, method_name, method):
+    def gen_method_call(self, var_name, method):
         method_arg_types = [
             arg_type.annotation
             for arg_type in inspect.signature(method).parameters.values()
         ]
-        return ast.Call(
-            func=ast.Attribute(
-                value=ast.Name(id=var_name, ctx=ast.Load()),
-                attr=method_name,
-                ctx=ast.Load(),
-            ),
+
+        instance_node = ast.Name(id=var_name, ctx=ast.Load())
+
+        attribute_node = ast.Attribute(
+            value=instance_node,
+            attr=method.__name__,
+            ctx=ast.Load(),
+        )
+
+        call_node = ast.Call(
+            func=attribute_node,
             args=[
                 self.gen_expression_with_type(arg_type)
                 for arg_type in method_arg_types
@@ -267,35 +332,79 @@ class ASTGenerator:
             keywords=[],
         )
 
-    def gen_expression_with_type(self, ty: Type):
+        return call_node
+
+    def get_leaf_expressions_of_type(self, ty: type):
+        """Return a list of valid AST nodes that do not have subexpressions."""
+
+        leaf_exprs = []
+
+        # First, gather all literals:
+        if ty in self.literals_by_type:
+            leaf_exprs.extend(
+                [ast.Constant(value=v) for v in self.literals_by_type[ty]]
+            )
+
+        # Then all variables of this type:
+        leaf_exprs.extend(
+            [
+                ast.Name(id=var_name, ctx=ast.Load())
+                for var_name, _ in self.cur_scope().get_terminals(ty)
+            ]
+        )
+
+        # Then all method calls that return this type and take no arguments (besides
+        # `self`).
+        leaf_exprs.extend(
+            [
+                self.gen_method_call(var_name, method)
+                for var_name, method in self.cur_scope().get_call_expressions(ty)
+                if _method_has_no_parameters(method)
+            ]
+        )
+
+        return leaf_exprs
+
+    def gen_expression_with_type(self, ty: type):
+
+        def __register(ty, node):
+            self.node_metadata[node] = NodeInfo(
+                scope=deepcopy(self.cur_scope()), expr_type=ty
+            )
+            return node
+
         self.expr_depth += 1
 
-        call_exprs = self.cur_scope().get_call_expressions(ty)
-        terminals = self.cur_scope().get_terminals(ty)
+        # A list of (name, method) tuples. We cannot turn these into AST nodes yet
+        # because the number of possible trees for a function call is very large.
+        call_exprs = [
+            (var_name, method)
+            for var_name, method in self.cur_scope().get_call_expressions(ty)
+            if not _method_has_no_parameters(method)
+        ]
 
-        # If no options are available, fall back to gen_constant_with_type, which will
-        # just pick a constant with type `ty` at random.
-        if (not call_exprs and not terminals) or (self.expr_depth > MAX_EXPR_DEPTH):
-            return self.gen_constant_with_type(ty)
+        # A list of AST nodes with no children.
+        terminals = self.get_leaf_expressions_of_type(ty)
 
         # Terminals have a 50% chance of being picked in the outermost scope, with
         # probability linearly increasing to 100% at maximum scope depth. If the
         # expression is already too nested, set the probability of a terminal to 100%.
-        terminal_probability = (1 - 0.5) / MAX_SCOPES * len(self.scopes) + 0.5
-        if self.expr_depth >= MAX_EXPR_DEPTH:
-            terminal_probability = 1.0
+        terminal_probability = (
+            1 - BASE_TERMINAL_PROBABILITY
+        ) / MAX_EXPR_DEPTH * self.expr_depth + BASE_TERMINAL_PROBABILITY
 
         # Pick either a terminal or a call expression. Depending on terminal
         # availability, return either a constant or an actual terminal (i.e. a varname).
         # If method, then populate the method's arguments appropriately.
-        if random.random() < terminal_probability and terminals:
-            term_name, _ = random.choice(terminals)
-            ret_node = ast.Name(id=term_name, ctx=ast.Load())
-        elif call_exprs:
-            var_name, method_name, method = random.choice(call_exprs)
-            ret_node = self.gen_method_call(var_name, method_name, method)
+        if random.random() < terminal_probability:
+            ret_node = __register(ty, random.choice(terminals))
         else:
-            ret_node = self.gen_constant_with_type(ty)
+            if call_exprs:
+                ret_node = __register(
+                    ty, self.gen_method_call(*random.choice(call_exprs))
+                )
+            else:
+                ret_node = __register(ty, random.choice(terminals))
 
         # Special case for bools, since and/or/not are not methods but control flow
         # operators handled directly in the interpreter. Bool expressions have a fixed
@@ -308,32 +417,19 @@ class ASTGenerator:
             bin_ops = [ast.And, ast.Or]
 
             if random.random() < PROB_OP_BOOL_EXPR_USES_NOT:
-                ret_node = ast.UnaryOp(op=ast.Not(), operand=ret_node)
+                ret_node = __register(ty, ast.UnaryOp(op=ast.Not(), operand=ret_node))
             else:
-                ret_node = ast.BoolOp(
-                    op=random.choice(bin_ops)(),
-                    values=[ret_node, self.gen_expression_with_type(bool)],
+                ret_node = __register(
+                    ty,
+                    ast.BoolOp(
+                        op=random.choice(bin_ops)(),
+                        values=[ret_node, self.gen_expression_with_type(bool)],
+                    ),
                 )
 
         self.expr_depth -= 1
 
-        # Register the node:
-        self.node_metadata[ret_node] = NodeInfo(
-            scope=deepcopy(self.cur_scope()), expr_type=ty
-        )
-
         return ret_node
-
-    def gen_constant_with_type(self, ty):
-        if getattr(ty, "__origin__", None) and ty.__origin__ == list:
-            return ast.Constant(value=[])
-
-        types = {
-            bool: [True, False],
-            float: [1.0],  # or maybe some generator function?
-            int: [1, 2],
-        }
-        return ast.Constant(value=random.choice(types[ty]))
 
     def gen_if(self):
 
@@ -347,7 +443,7 @@ class ASTGenerator:
 
             # Enter scope of the "else" portion
             self.enter_scope()
-            if_stmt.orelse = [self.gen_statement()]
+            if_stmt.orelse = [self.gen_statements()]
             self.exit_scope()
         else:
             self.exit_scope()
@@ -362,34 +458,27 @@ class ASTGenerator:
 
         # Generate an expression to assign to this variable
         # TODO: Better heuristics
-        var_name, method_name, method = random.choice(
-            self.cur_scope().get_call_expressions()
-        )
+        all_types = self.cur_scope().get_all_types()
+        var_type = random.choice(all_types)
+
         node = ast.Assign(
             targets=[ast.Name(id=lhs)],
-            value=self.gen_method_call(var_name, method_name, method),
+            value=self.gen_expression_with_type(var_type),
         )
-        self.add_var_to_current_scope(lhs, inspect.signature(method).return_annotation)
+
+        self.add_var(lhs, var_type)
         return node
 
     def gen_for_loop(self):
-        # Get list of method calls that return some sort of list
-        call_exprs = self.cur_scope().get_calls_with_predicate(
-            lambda method: getattr(
-                inspect.signature(method).return_annotation, "__origin__", None
-            )
-            == list
-        )
-        terminals = [
-            (var_name, ty)
-            for var_name, ty in self.cur_scope().get_terminals()
-            if getattr(ty, "__origin__", None) == list
-        ]
+
+        self.loop_depth += 1
 
         # Set of all container types available to the generator at this point
         container_types = [
-            inspect.signature(method).return_annotation for _, _, method in call_exprs
-        ] + [ty for _, ty in terminals]
+            ty
+            for ty in self.cur_scope().get_all_types()
+            if getattr(ty, "__origin__", None) == list
+        ]
 
         # Choose a type to generate an expression for.
         container_type = random.choice(container_types)
@@ -400,32 +489,36 @@ class ASTGenerator:
 
         self.enter_scope()
         loop_var = self.get_var_name()
-        self.add_var_to_current_scope(loop_var, iterator_type)
+        self.add_var(loop_var, iterator_type)
 
         for_loop = ast.For(
             target=ast.Name(id=loop_var, ctx=ast.Store()),
             iter=container_expr,
-            body=[self.gen_statement()],
+            body=[self.gen_statements()],
             orelse=[],
         )
 
         self.exit_scope()
+        self.loop_depth += 1
+
         return for_loop
 
     def gen_statement(self):
-        non_complex_stmts = [self.gen_variable_assignment]
-
-        complex_stmts = [
-            self.gen_if,
-            self.gen_for_loop,
-        ]
+        stmts = [self.gen_variable_assignment]
+        complex_stmts = [self.gen_if]
 
         non_complex_prob = 0.5 + (1 - 0.5) * (len(self.scopes) / 4)
 
+        if self.loop_depth < MAX_LOOP_DEPTH:
+            complex_stmts.append(self.gen_for_loop)
+
         stmt_func = random.choice(
-            non_complex_stmts if random.random() < non_complex_prob else complex_stmts
+            stmts if random.random() < non_complex_prob else complex_stmts
         )
-        return stmt_func()
+
+        node = stmt_func()
+        ast.fix_missing_locations(node)
+        return node
 
     def gen_statements(self):
         return [self.gen_statement() for _ in range(random.randint(1, 3))]
@@ -433,15 +526,60 @@ class ASTGenerator:
     def gen_module(self):
         module_node = ast.Module(body=self.gen_statements(), type_ignores=[])
         ast.fix_missing_locations(module_node)
+
+        self.root = module_node
         return module_node
 
+    def mutate(self):
+        self.root = self.visit(self.root)
+        return self.root
 
-for i in range(10000):
+    def generic_visit(self, node):
+        return super().generic_visit(node)
+
+    def visit_Assign(self, node):
+        # TODO: Assignment statement deletion
+        metadata = self.node_metadata[node.value]
+        scope = metadata.scope
+
+        var_name = node.targets[0].id
+        var_type = metadata.expr_type
+
+        # XXX: This results in a scope depth bug because generated expressions don't
+        # know how deep we are.
+        self.scopes.append(scope)
+        new_value = self.gen_expression_with_type(var_type)
+
+        new_node = ast.Assign(
+            targets=[ast.Name(id=var_name, ctx=ast.Store())],
+            value=new_value,
+        )
+        ast.fix_missing_locations(new_node)
+        self.scopes.pop()
+        return new_node
+
+
+for i in range(1000):
     print(i)
     a = ASTGenerator()
-    code = ast.unparse(a.gen_module())
-    print(code)
-    input()
+    try:
+        code = ast.unparse(a.gen_module())
+
+        foo = Foo()
+        namespace = {"foo": foo}
+
+        print("===========================")
+        print("Old code")
+        print(code)
+        exec(code, namespace)
+
+        new_code = ast.unparse(a.mutate())
+        print("\nNew code:")
+        print(new_code)
+        exec(new_code, namespace)
+    except RecursionError:
+        print("Recursion error!")
+        exit(1)
     try:
         compile(code, "<text>", "exec")
     except Exception as e:
